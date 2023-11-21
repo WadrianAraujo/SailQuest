3ea044a5cdbdb211e58e4f80cd40b2a19624e5c1
Wadrian Araujo De Moura <wadm.snf20@uea.edu.br>
2023-11-21 17:42:52 -0400

    add player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header("Boat Specs")]
+        [SerializeField] private float maxSpeed = 5f;
+        [SerializeField] private float acceleration = 2f;
+        [SerializeField] private float drag = 0.1f;
+
+        private Rigidbody2D _rigidbody2Drb;
+        private float currentSpeed;
+        private float currentAngle;
+
+        void Start()
+        {
+            _rigidbody2Drb = GetComponent<Rigidbody2D>();
+            _rigidbody2Drb.gravityScale = 0f; // Disable gravity for 2D movement
+            _rigidbody2Drb.drag = drag;
+        }
+
+        void FixedUpdate()
+        {
+            float rotation = -Input.GetAxis("Horizontal");
+            float movement = Input.GetAxis("Vertical");
+            
+            UpdateRotation(rotation);
+            UpdateSpeed(movement);
+            
+            Vector2 force = transform.up * (currentSpeed * acceleration);
+            _rigidbody2Drb.AddForce(force);
+        }
+
+        void UpdateRotation(float rotation)
+        {
+            currentAngle += rotation * Time.deltaTime * acceleration;
+            transform.rotation = Quaternion.Euler(0, 0, currentAngle);
+        }
+
+        void UpdateSpeed(float movement)
+        {
+            currentSpeed += movement * acceleration * Time.deltaTime;
+            currentSpeed = Mathf.Clamp(currentSpeed, -maxSpeed, maxSpeed);
+        }
+    }
+}
+
